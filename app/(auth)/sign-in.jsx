79736d1a5dc70f62f3b36ab24df3e42fcf6a0c0f
mrtfog <fogliaccomartin3@gmail.com>
2024-07-15 19:28:52 -0300

    fix: user session active redirect eorrr

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,50 +1,60 @@
 import { useState } from "react";
+import { Link, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Alert, Image, ScrollView, Text, View } from "react-native";
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
+
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link, router } from "expo-router";
 import { getCurrentUser, signIn } from "../../lib/appwrite";
 import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignIn = () => {
-  const { setIsLogged, setUser } = useGlobalContext();
+  const { setUser, setIsLogged } = useGlobalContext();
+  const [isSubmitting, setSubmitting] = useState(false);
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
-  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const submit = async () => {
-    if (!form.email || !form.password) {
-      Alert.alert("Error", "Please fill in all the fields");
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
     }
-    setIsSubmitting(true);
+
+    setSubmitting(true);
+
     try {
       await signIn(form.email, form.password);
       const result = await getCurrentUser();
       setUser(result);
       setIsLogged(true);
 
+      Alert.alert("Success", "User signed in successfully");
       router.replace("/home");
     } catch (error) {
       Alert.alert("Error", error.message);
     } finally {
-      setIsSubmitting(false);
+      setSubmitting(false);
     }
   };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center min-h-[85vh] px-4 my-6 ">
+        <View
+          className="w-full flex justify-center h-full px-4 my-6"
+          style={{
+            minHeight: Dimensions.get("window").height - 100,
+          }}
+        >
           <Image
             source={images.logo}
             resizeMode="contain"
-            className="w-[115px] h-[35px]"
+            className="w-[115px] h-[34px]"
           />
 
-          <Text className="text-2xl text-white text-semibold font-psemibold mt-10">
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
             Log in to Aora
           </Text>
 
@@ -70,15 +80,15 @@ const SignIn = () => {
             isLoading={isSubmitting}
           />
 
-          <View className=" justify-center pt-5 flex-row gap-2">
+          <View className="flex justify-center pt-5 flex-row gap-2">
             <Text className="text-lg text-gray-100 font-pregular">
-              Don't have account?
+              Don't have an account?
             </Text>
             <Link
-              href={"/sign-up"}
+              href="/sign-up"
               className="text-lg font-psemibold text-secondary"
             >
-              Sign Up
+              Signup
             </Link>
           </View>
         </View>
