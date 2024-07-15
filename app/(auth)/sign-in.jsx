5acb3e4be1b40cdfb20d02e4b76fcd385cc7984e
mrtfog <fogliaccomartin3@gmail.com>
2024-07-15 17:28:54 -0300

    feat: sign in & sign up pages

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Image, ScrollView, Text, View } from "react-native";
-import FormField from "../../components/FormField";
 import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6 ">
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6 ">
           <Image
             source={images.logo}
             resizeMode="contain"
@@ -30,13 +35,32 @@ const SignIn = () => {
             otherStyles="mt-7"
             keyboardType="email-address"
           />
+
           <FormField
             title="Password"
             value={form.password}
-            handleChangeText={(e) => setForm({ ...form, email: e })}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles="mt-7"
-            keyboardType="password-address"
           />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className=" justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+            </Text>
+            <Link
+              href={"/sign-up"}
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
